8e752a76587668e00b3a5af9a8c40ca4b16c0029
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-05-18 17:43:22 +0200

    Create services and db classes directly in constructor parameters

--- src/main/java/fr/xebia/App.java
+++ src/main/java/fr/xebia/App.java
@@ -5,14 +5,10 @@ import net.codestory.http.WebServer;
 public class App {
 
     public static void main( String[] args ) {
-
-        UserDB userDB = new UserDB();
-        SlotService slotService = new SlotService(new SlotDB(), new SlotMailer());
-
         new WebServer().
                 configure(routes -> routes
-                                .add(new UserResource(userDB))
-                                .add(new SlotResource(slotService))
+                                .add(new UserResource(new UserDB()))
+                                .add(new SlotResource(new SlotService(new SlotDB(), new SlotMailer())))
                 ).start();
     }
 }
